6f5a1678910d9549d49b8ce2a73d9f98d576bf83
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2023-03-22 20:38:26 +0100

    Made the Project responsive and add new features

---
+++ public/script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.getElementById('menu-button')
+const mobileNav = document.querySelector('.mobile-menu')
+const closeBtn = document.getElementById('close-button')
+const signOnBtn = document.getElementById('sign-on-btn')
+const signOnPage = document.querySelector('.sign-on')
+
+menuBtn.addEventListener('click', () => {
+    mobileNav.classList.add('open')
+    menuBtn.style.display = 'none'
+    closeBtn.style.display = 'block'
+})
+closeBtn.addEventListener('click', () => {
+    mobileNav.classList.remove('open')
+    closeBtn.style.display = 'none'
+    menuBtn.style.display = 'block'
+})
+
+signOnBtn.addEventListener('click', () => {
+    signOnPage.classList.toggle('open')
+})
