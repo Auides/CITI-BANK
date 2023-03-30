a6f89a0df41f518d06b78169a4a6ff764a39ef52
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2023-03-30 23:05:30 +0100

    Created a Sign Up Page and made it responsive

--- public/script.js
+++ public/script.js
@@ -1,20 +1,22 @@
-const menuBtn = document.getElementById('menu-button')
-const mobileNav = document.querySelector('.mobile-menu')
-const closeBtn = document.getElementById('close-button')
-const signOnBtn = document.getElementById('sign-on-btn')
-const signOnPage = document.querySelector('.sign-on')
+const menuBtn = document.getElementById('menu-button');
+const mobileNav = document.querySelector('.mobile-menu');
+const closeBtn = document.getElementById('close-button');
+const signOnBtn = document.getElementById('sign-on-btn');
+const signOnPage = document.querySelector('.sign-on');
 
 menuBtn.addEventListener('click', () => {
-    mobileNav.classList.add('open')
-    menuBtn.style.display = 'none'
-    closeBtn.style.display = 'block'
+    mobileNav.classList.add('open');
+    menuBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+    signOnPage.classList.remove('open');
 })
 closeBtn.addEventListener('click', () => {
-    mobileNav.classList.remove('open')
-    closeBtn.style.display = 'none'
-    menuBtn.style.display = 'block'
+    mobileNav.classList.remove('open');
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'block';
 })
 
 signOnBtn.addEventListener('click', () => {
-    signOnPage.classList.toggle('open')
+    signOnPage.classList.toggle('open');
+    mobileNav.classList.remove('open');
 })
